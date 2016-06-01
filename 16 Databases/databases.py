8d7391b7937cfff6c6b3258215d57fd062188f31
Megamala <jacobrwilson@yahoo.com>
2016-06-01 14:43:39 -0400

    Leaving for day

---
+++ 16 Databases/databases.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def main():
+    db = sqlite3.connect('test1.db')
+    db.row_factory = sqlite3.Row
+    # shows row objects instead of the row values
+    db.execute('drop table if exists test')
+    db.execute('create table test (t1 text, i1 int)')
+    db.execute('insert into test (t1, i1) values (?,?)', ('one', 1))
+    db.execute('insert into test (t1, i1) values (?,?)', ('two', 2))
+    db.execute('insert into test (t1, i1) values (?,?)', ('three', 3))
+    db.execute('insert into test (t1, i1) values (?,?)', ('four', 4))
+    db.commit()
+    cursor = db.execute('select i1, t1 from test order by i1')
+    for row in cursor:
+        print(dict(row), row, row['t1'], row['i1'])
+
+
+if __name__ == "__main__":
+    main()
